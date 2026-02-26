60d8a81058232107256d32039159b27f1afad6f3
COLDESTBOW30654 <2724008496@qq.com>
2026-02-26 15:42:33 +0800

    改了下备用链接

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blog.119567.xyz/",
+	site: "https://blog.blym.top/",
 	base: "/",
 	trailingSlash: "never",
 	integrations: [
